555f92359fb4cbff66b792dff593e8b8a519cecf
Christian Aceves <acevesrocha2009@gmail.com>
2026-05-09 16:47:22 +0200

    Fix scrolling problem

--- src/components/layout/NewsSection.tsx
+++ src/components/layout/NewsSection.tsx
@@ -1,5 +1,3 @@
-import "./news-section.css";
-
 function NewsSection({ children }: { children: any }) {
   return <div className="news-section">{children}</div>;
 }
--- src/pages/News.tsx
+++ src/pages/News.tsx
@@ -10,6 +10,7 @@ import NewImage from "../components/new/NewImage";
 import NewText from "../components/new/NewText";
 import NoNewsText from "../components/layout/no-news/NoNewsText";
 import NewsSearchBar from "../components/news-search-bar/NewsSearchBar";
+import "./news.css";
 
 function News() {
   const [searchTerm, setSearchTerm] = useState("");
@@ -26,32 +27,34 @@ function News() {
   return (
     <div className="news">
       <NewsSearchBar value={searchTerm} onChange={setSearchTerm} />
-      <NewsSection>
-        {filteredNews.length > 0 ? (
-          filteredNews.map((news) => (
-            <Link
-              key={news.id}
-              to={`/news/${news.id}`}
-              style={{ textDecoration: "none", color: "inherit" }}
-            >
-              <New category={categoryLabels[news.category]}>
-                <NewContent>
-                  <NewTitle>{news.title}</NewTitle>
-                </NewContent>
-                <NewContent>
-                  <NewSummary>{news.summary}</NewSummary>
-                  <NewImage image_src={news.image} />
-                  {news.texts.slice(0, 3).map((text, index) => (
-                    <NewText key={index}>{text}</NewText>
-                  ))}
-                </NewContent>
-              </New>
-            </Link>
-          ))
-        ) : (
-          <NoNewsText />
-        )}
-      </NewsSection>
+      <div className="news__content">
+        <NewsSection>
+          {filteredNews.length > 0 ? (
+            filteredNews.map((news) => (
+              <Link
+                key={news.id}
+                to={`/news/${news.id}`}
+                style={{ textDecoration: "none", color: "inherit" }}
+              >
+                <New category={categoryLabels[news.category]}>
+                  <NewContent>
+                    <NewTitle>{news.title}</NewTitle>
+                  </NewContent>
+                  <NewContent>
+                    <NewSummary>{news.summary}</NewSummary>
+                    <NewImage image_src={news.image} />
+                    {news.texts.slice(0, 3).map((text, index) => (
+                      <NewText key={index}>{text}</NewText>
+                    ))}
+                  </NewContent>
+                </New>
+              </Link>
+            ))
+          ) : (
+            <NoNewsText />
+          )}
+        </NewsSection>
+      </div>
     </div>
   );
 }
